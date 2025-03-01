f99117c37ba432c1b78703027116f560a1679b8e
Proverbs35 <ncancel@fingerlakes.edu>
2025-02-28 19:14:37 -0500

    Update inches_to_feet.py
    Nate Cancel

--- inches_to_feet.py
+++ inches_to_feet.py
@@ -1,64 +1,47 @@
 # FILE NAME - inches_to_feet.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-
-
+# NAME: Nate Cancel
+# DATE: 02-28-2025
+# BRIEF DESCRIPTION:  make inches into feet
 
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions below
 # 4. The Sample Output has been included in this code for your convenience
 
-
-
-
-
 # BEWARE! You'll need to cast not only the input from the user
 # but also maybe the number of feet
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
     
-    
-    
-    
-    
-    
-    
-    
+convert(inches to feet)
+inches = float(input('Enter the number of inches: '))
+feet = inches // 12
+remaining_inches = inches % 12
+
+print(f" {inches} inches is {feet} feet, and {remaining_inches:.2f} inches. ")             
+        
     
     
 ########### END YER CODE ABOVE THIS LINE ###########
-
-
-
-
-
+ 
 ########################################
 #          SAMPLE OUTPUT
 ########################################
 
 '''
-
 Enter the number of inches: 14
 
 14 inches is 1 feet, and 2 inches
 
-
 '''
 
-
-
 '''
-
 Enter the number of inches: 100
 
 100 inches is 8 feet, and 4 inches
 
 '''
-
-
 
 ########################################
 #          REFLECTION QUESTIONS
@@ -67,8 +50,7 @@ Enter the number of inches: 100
 '''
 
 1. What does it mean to "cast" input from the user?
-
-
+ converting input into something we can use for the code
 
 
 
