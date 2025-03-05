fb535f01374b27912b984684ef117388ca9b9e33
Proverbs35 <ncancel@fingerlakes.edu>
2025-03-05 18:15:45 -0500

    Update inches_to_feet.py

--- inches_to_feet.py
+++ inches_to_feet.py
@@ -14,10 +14,9 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-inches = int(input('Enter the number of inches: '))
+inches = int(input('Enter the number of inches:'))
 feet = inches // 12
 remaining_inches = inches % 12
-print()
 print(f'{inches} inches is {feet} feet, and {remaining_inches} inches')
 print()
 
